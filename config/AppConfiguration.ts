8ef1cfd966328a0c060b3d3bd1ea26b8a8014175
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-03-31 17:48:35 +0530

    App Configured and Color Pallete also added

---
+++ config/AppConfiguration.ts
@@ -0,0 +1,3 @@
+export default {
+  BaseURL: "https://www.instagram.com/",
+};
---
+++ config/ColorPallete.ts
@@ -0,0 +1,11 @@
+export default {
+  primary: "#1E90FF",
+  black: "black",
+  red: "#ED383F",
+  white: "#FFFFFF",
+  border: "#D3D3D3",
+  notification: "rgb(255, 69, 58)",
+  statusBarColor: "#096aab",
+  card: "orangered",
+  DateColor: "#34b7f1",
+};
