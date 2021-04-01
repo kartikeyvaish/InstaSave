0853848ccd5c5d4a8844cdfc7928abc184620cd5
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-04-01 12:15:12 +0530

    App.js configured with modes and everything

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import AppLoading from "expo-app-loading";
+import { Root } from "native-base";
+import { Appearance, AppearanceProvider } from "react-native-appearance";
+
+import AppNavigator from "./navigation/AppNavigator";
+import Theme from "./themes/Theme";
+import useFonts from "./hooks/useFonts";
+
+function GetTheme(Mode) {
+  return Mode === "Default" ? Theme[Appearance.getColorScheme()] : Theme[Mode];
+}
+const defaultScheme = Appearance.getColorScheme();
 
 export default function App() {
+  const [IsReady, SetIsReady] = useState(false);
+  const [Mode, SetMode] = useState(defaultScheme);
+
+  useEffect(() => {
+    const subscription = Appearance.addChangeListener(({ colorScheme }) => {
+      SetMode(colorScheme);
+    });
+    return () => subscription.remove();
+  }, []);
+
+  const LoadFonts = async () => {
+    await useFonts();
+  };
+
+  if (!IsReady) {
+    return (
+      <AppLoading
+        startAsync={LoadFonts}
+        onFinish={() => SetIsReady(true)}
+        onError={() => {}}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppearanceProvider>
+      <NavigationContainer theme={GetTheme(Mode)}>
+        <Root>
+          <AppNavigator />
+        </Root>
+      </NavigationContainer>
+    </AppearanceProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
