fffed5e11865e02b9f9315428d1cc0700e32d389
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-04-01 12:11:29 +0530

    Created Downloading Screen

---
+++ screens/DownloadingScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+function DownloadingScreen(props) {
+  return <View style={styles.container}></View>;
+}
+
+export default DownloadingScreen;
+
+const styles = StyleSheet.create({
+  container: {},
+});
