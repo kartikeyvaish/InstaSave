7439e7883e985a1cd387e91552cb8cee3a97c8cb
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-04-13 00:23:35 +0530

    Post API for Reels

--- api/Insta.ts
+++ api/Insta.ts
@@ -1,6 +1,18 @@
 import apiClient from "./Client";
 
 const GetMediaData = (uri: any) =>
+  apiClient.get(
+    uri,
+    {},
+    {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0",
+      },
+    }
+  );
+
+const GetMediaDataPOST = (uri: any) =>
   apiClient.post(
     uri,
     {},
@@ -12,6 +24,6 @@ const GetMediaData = (uri: any) =>
     }
   );
 
-export default { GetMediaData };
+export default { GetMediaData, GetMediaDataPOST };
 
 // Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36
